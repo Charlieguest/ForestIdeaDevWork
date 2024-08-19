3cb83e1ddf98a54e1254dca4c4d0a6acef937621
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2024-08-19 23:22:26 +0000

    Folder Switcharoo

--- ForestProject/Intermediate/Build/Win64/x64/ForestProjectEditor/Development/Engine/SharedPCH.Engine.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Github/ForestIdeaDevWork/ForestProject/Intermediate/Build/Win64/x64/ForestProjectEditor/Development/Engine/SharedDefinitions.Engine.Cpp20.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
--- ForestProject/Intermediate/Build/Win64/x64/ForestProjectEditor/Development/UnrealEd/SharedPCH.UnrealEd.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Editor/UnrealEd/Public/UnrealEdSharedPCH.h
-#include "D:/Github/ForestIdeaDevWork/ForestProject/Intermediate/Build/Win64/x64/ForestProjectEditor/Development/UnrealEd/SharedDefinitions.UnrealEd.Cpp20.h"
-#include "Editor/UnrealEd/Public/UnrealEdSharedPCH.h"
