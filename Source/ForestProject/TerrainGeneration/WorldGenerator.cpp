7aa75d14c6d5717bd6867ea335c6dd8d88b960d8
Charlieguest <114992920+Charlieguest@users.noreply.github.com>
2024-09-03 14:18:48 +0000

    Started moving functionality over to C++

---
+++ Source/ForestProject/TerrainGeneration/WorldGenerator.cpp
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WorldGenerator.h"
+
+
+// Sets default values
+AWorldGenerator::AWorldGenerator()
+{
+	_ProcTerrain = CreateDefaultSubobject<UProceduralMeshComponent>(TEXT("TerrainMesh"));
+	_ProcTerrain->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AWorldGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AWorldGenerator::GenerateTerrain(int inSectionX, int inSectionY)
+{
+	
+}
+
---
+++ Source/ForestProject/TerrainGeneration/WorldGenerator.h
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ProceduralMeshComponent.h"
+
+#include "WorldGenerator.generated.h"
+
+UCLASS()
+class FORESTPROJECT_API AWorldGenerator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWorldGenerator();
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int YVertexCount = 50;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int XVertexCount = 50;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	float CellSize = 500.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int NumOfSectionsX = 2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	int NumOfSectionsY = 2;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int MeshSectionIndex = 0;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UProceduralMeshComponent> _ProcTerrain;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<UMaterialInterface> _ProcTerrianMat = nullptr;
+	
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintCallable)
+	void GenerateTerrain(int inSectionX, int inSectionY);
+};
